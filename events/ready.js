30621443f838b1faed2318e98c8e13d0507334a5
elmanueh <bernabe2003@gmail.com>
2024-03-03 21:29:35 +0100

    New tables created

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ import { databaseUpdate } from '../src/events/databaseUpdate.js';
 import { discordGuildId } from '../src/services/discord.js';
 import { RequestCountClashOfClansCreate } from '../src/services/clashofclansAPI.js';
 import { writeConsoleANDLog } from '../src/write.js';
+import { createDatabase } from '../src/services/database.js';
 
 export default {
 	name: Events.ClientReady,
@@ -11,7 +12,11 @@ export default {
 	async execute(client) {
 		await writeConsoleANDLog('-------------------------------------------------');
 		await writeConsoleANDLog(`Conectado correctamente como '${client.user.tag}'`);
-		
+
+		try {
+			await createDatabase();
+		} catch (error) { return await writeConsoleANDLog('PLEASE, RESTART THE BOT'); }
+
 		// This execute the code to manage the database update with information about wars.
 		currentWar();
 		// This execute the code to manage the update of users clan.
--- src/services/database.js
+++ src/services/database.js
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3';
 import { DatabaseError } from '../errorCreate.js';
+import { writeConsoleANDLog } from '../write.js';
 const db = new sqlite3.Database('./mybotdata.sqlite');
 
 export async function databaseGet(solicitudDB) {
@@ -24,7 +25,83 @@ export async function databaseRun(solicitudDB) {
     return new Promise((resolve, reject) => {
         db.run(solicitudDB, function(err) {
             if (!err) return resolve(true);
-            return reject(new DatabaseError(err));
+            return reject(DatabaseError(err));
         })
     });
+}
+
+export async function createDatabase() {
+    const TablePlayerData = `
+        CREATE TABLE IF NOT EXISTS PlayerData (
+            tag                 TEXT    NOT NULL,
+            name                TEXT    NOT NULL,
+            townHall            TEXT    NOT NULL,
+            warPreference       TEXT    NOT NULL,
+            PRIMARY KEY (tag)
+        );
+    `;
+    const TableClanData = `
+        CREATE TABLE IF NOT EXISTS ClanData (
+            tag                 TEXT    NOT NULL,
+            lastUpdate          TEXT,
+            lastWar             TEXT,
+            PRIMARY KEY (tag)
+        );
+    `;
+    const TablePlayerClanData = `
+        CREATE TABLE IF NOT EXISTS PlayerClanData (
+            clan                TEXT    NOT NULL,
+            player              TEXT 	NOT NULL,
+            role                TEXT    NOT NULL,
+            warAttacks          TEXT 	NOT NULL    DEFAULT '- - - - -',
+            PRIMARY KEY (clan, player),
+            CONSTRAINT fk_clan FOREIGN KEY (clan) REFERENCES ClanData(tag)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+            CONSTRAINT fk_player FOREIGN KEY (player) REFERENCES PlayerData(tag)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        );
+    `;
+    const TableUserConnections = `
+        CREATE TABLE IF NOT EXISTS UserConnections (
+            discordId           TEXT    NOT NULL,
+            playerTag           TEXT    NOT NULL,
+            PRIMARY KEY (discordId, playerTag),
+            CONSTRAINT fk_playerTag FOREIGN KEY (playerTag) REFERENCES PlayerData(tag)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        );
+    `;
+    const TableGuildConnections = `
+        CREATE TABLE IF NOT EXISTS GuildConnections (
+            guildId             TEXT    NOT NULL,
+            clanTag             TEXT    NOT NULL,
+            channelLogId        TEXT,
+            notMemberRoleId     TEXT,
+            memberRoleId        TEXT,
+            adminRoleId         TEXT,
+            coLeaderRoleId      TEXT,
+            leaderRoleId        TEXT,
+            PRIMARY KEY (guildId, clanTag),
+            CONSTRAINT fk_clanTag FOREIGN KEY (clanTag) REFERENCES ClanData(tag)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+        );
+    `;
+
+    try {
+        await databaseRun('PRAGMA foreign_keys = ON');
+        await databaseRun('BEGIN');
+        await databaseRun(TablePlayerData);
+        await databaseRun(TableClanData);
+        await databaseRun(TablePlayerClanData);
+        await databaseRun(TableUserConnections);
+        await databaseRun(TableGuildConnections);
+        await databaseRun('COMMIT');
+    } catch (error) {
+        await databaseRun('ROLLBACK');
+        await writeConsoleANDLog(error);
+        throw error;
+    }
 }
