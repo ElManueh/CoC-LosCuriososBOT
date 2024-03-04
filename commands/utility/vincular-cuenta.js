eda333339b6dc22d4e8c447bd207dea3f114ad87
elmanueh <bernabe2003@gmail.com>
2024-03-05 00:18:00 +0100

    Update command LinkAccount

--- commands/utility/vincular-cuenta.js
+++ commands/utility/vincular-cuenta.js
@@ -1,9 +1,9 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { getUserInfo, verifyUserToken } from '../../src/services/clashofclansAPI.js';
-import { databaseGet, databaseRun } from '../../src/services/database.js';
-import { discordNameUpdate, discordRoleUpdate, discordChannelLog } from '../../src/services/discord.js';
+import { getPlayer, verifyPlayerToken } from '../../src/services/clashofclansAPI.js';
+import { databaseAll, databaseRun } from '../../src/services/database.js';
 import mensajes from '../../src/locale.json' assert { type: 'json' };
 import { writeConsoleANDLog } from '../../src/write.js';
+import { ClashOfClansError, SQLITE_CONSTRAINT_FOREIGNKEY, SQLITE_CONSTRAINT_UNIQUE } from '../../src/errorCreate.js';
 
 export default {
 	category: 'utility',
@@ -20,44 +20,51 @@ export default {
 			.setRequired(true)),
 	async execute(interaction) {
 		try {
-			const optionUserTag = interaction.options.getString('usuario-tag');
-			const optionUserApi = interaction.options.getString('codigo-api');
-			
-			let userDatabase = await databaseGet(`SELECT * FROM usuariosCOC WHERE discordID = '${interaction.user.id}'`);
-			if (userDatabase) return interaction.reply({ content: mensajes.clashofclans.discord_ya_vinculado, ephemeral: true });
+			const optionPlayerTag = interaction.options.getString('usuario-tag');
+			const optionPlayerToken = interaction.options.getString('codigo-api');
 
-			let userClan = await getUserInfo(optionUserTag);
-			let tokenVerified = await verifyUserToken(optionUserTag, optionUserApi);
+			let playerClan = await getPlayer(optionPlayerTag);
+			let tokenVerified = await verifyPlayerToken(optionPlayerTag, optionPlayerToken);
 			if (!tokenVerified) return interaction.reply({ content: mensajes.clashofclans.api_incorrecta, ephemeral: true });
 
-			userDatabase = await databaseGet(`SELECT * FROM usuariosCOC WHERE tag = '${userClan.tag}'`);
-			if (userDatabase.length !== 0 && userDatabase.discordID != null) return interaction.reply({ content: mensajes.clashofclans.tag_ya_vinculado, ephemeral: true });
-
-			if (userDatabase.length === 0)
-				await databaseRun(`INSERT INTO usuariosCOC (discordID, tag, nombre, rango) 
-									VALUES ('${interaction.user.id}', '${userClan.tag}', '${userClan.name}', '${userClan.role}')`);
-			else
-				await databaseRun(`UPDATE usuariosCOC set discordID = '${interaction.user.id}' WHERE tag = '${userDatabase.tag}'`);
-
+			await databaseRun('BEGIN');
+			try {
+				await databaseRun(`INSERT INTO UserConnections VALUES ('${interaction.user.id}', '${playerClan.tag}')`);
+			} catch (error) {
+				if (error.code === SQLITE_CONSTRAINT_UNIQUE) return await interaction.reply({ content: mensajes.clashofclans.tag_ya_vinculado, ephemeral: true });
+				if (error.code === SQLITE_CONSTRAINT_FOREIGNKEY) {
+					await databaseRun(`INSERT INTO PlayerData VALUES ('${playerClan.tag}', '${playerClan.name}', '${playerClan.townHallLevel}', '${playerClan.warPreference}')`);
+					await databaseRun(`INSERT INTO UserConnections VALUES ('${interaction.user.id}', '${playerClan.tag}')`);
+				}
+			}
+			await databaseRun('COMMIT');
 			await interaction.reply({ content: mensajes.clashofclans.vinculado_ok, ephemeral: true });
-			await discordNameUpdate(interaction.user.id, userClan.name, interaction.guild);
-			await discordRoleUpdate(interaction.user.id, userClan.role, interaction.guild);
-
-			if (interaction.guild.ownerId === interaction.user.id) await interaction.followUp({ content: mensajes.discord.modificar_nombre_owner, ephemeral: true });
 
 			const messageEmbedLog = new EmbedBuilder()
 				.setColor(0x00FF00)
 				.addFields(
 					{ name: 'Discord', value: `${interaction.user.tag}`, inline: true },
-					{ name: 'Nombre CoC', value: `${userClan.name}`, inline: true },
-					{ name: 'Tag CoC', value: `${userClan.tag}`, inline: true },
+					{ name: 'PlayerName', value: `${playerClan.name}`, inline: true },
+					{ name: 'PlayerTag', value: `${playerClan.tag}`, inline: true },
 				)
 				.setTimestamp()
 				.setFooter({ text: `${interaction.user.id}`, iconURL: `${interaction.user.avatarURL()}` });
-		
-			const channel = interaction.guild.channels.cache.get(discordChannelLog);
-			await channel.send({ embeds: [messageEmbedLog]});
+
+			let clanConnecteds = await databaseAll(`SELECT * FROM GuildConnections WHERE clan = '${playerClan.tag}'`);
+			for (const connection of clanConnecteds) {
+				if (!connection.channelLogId) continue;
+				
+				let guild = interaction.client.guilds.cache.get(connection.guild);
+				if (!guild) await interaction.client.guilds.fetch(connection.guild);
+				let channel = guild.channels.cache.get(connection.channelLogId);
+				if (!channel) guild.channels.fetch(connection.channelLogId);
+
+				await channel.send({ embeds: [messageEmbedLog] });
+			}
 		} catch (error) {
+			if (error instanceof ClashOfClansError) {
+				if (error.errno === 404) return await interaction.reply({ content: mensajes.clashofclans.tag_incorrecto, ephemeral: true });
+			}
 			await interaction.reply({ content: mensajes.error.notificar, ephemeral: true });
 			await writeConsoleANDLog(error);
 		}
--- src/errorCreate.js
+++ src/errorCreate.js
@@ -1,20 +1,39 @@
 function getClashOfClansErrorMessage(error) {
-    let codeError = error.response.status;
-    if (codeError === 400) return 'Client provided incorrect parameters for the request.';
-    if (codeError === 403) return 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.';
-    if (codeError === 404) return 'Resource was not found.';
-    if (codeError === 429) return 'Request was throttled, because amount of requests was above the threshold defined for the used API token.';
-    if (codeError === 500) return 'Unknown error happened when handling the request.';
-    if (codeError === 503) return 'Service is temprorarily unavailable because of maintenance.';
-    if (codeError === 504) return 'The server did not get a response in time from the upstream server that it needed in order to complete the request.'
+    let errno = error.response.status;
+    if (errno === 400) return 'Client provided incorrect parameters for the request.';
+    if (errno === 403) return 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.';
+    if (errno === 404) return 'Resource was not found.';
+    if (errno === 429) return 'Request was throttled, because amount of requests was above the threshold defined for the used API token.';
+    if (errno === 500) return 'Unknown error happened when handling the request.';
+    if (errno === 503) return 'Service is temprorarily unavailable because of maintenance.';
+    if (errno === 504) return 'The server did not get a response in time from the upstream server that it needed in order to complete the request.'
 }
 
 export class ClashOfClansError extends Error {
     constructor (error) {
         super(getClashOfClansErrorMessage(error));
         this.name = 'CLASHOFCLANS_ERROR';
-        this.code = error.response.status;
+        this.errno = error.response.status;
         this.stack = null;
+    }
+}
+
+const SQLITE_CONSTRAINT = 19;
+export const SQLITE_CONSTRAINT_FOREIGNKEY = 'SQLITE_CONSTRAINT_FOREIGNKEY';
+export const SQLITE_CONSTRAINT_UNIQUE = 'SQLITE_CONSTRAINT_UNIQUE';
+
+function getDatabaseError(error) {
+    let errno = error.errno;
+    switch(errno) {  
+        case SQLITE_CONSTRAINT:
+            let message = error.message;
+            if (message.indexOf('FOREIGN KEY') !== -1) {
+                return SQLITE_CONSTRAINT_FOREIGNKEY;
+            } else if (message.indexOf('UNIQUE') !== -1) {
+                return SQLITE_CONSTRAINT_UNIQUE;
+            }
+        default:
+            return 'SQLITE_ERROR';
     }
 }
 
@@ -22,7 +41,8 @@ export class DatabaseError extends Error {
     constructor (error) {
         super(error.message);
         this.name = 'DATABASE_ERROR';
-        this.code = error.errno;
+        this.errno = error.errno;
+        this.code = getDatabaseError(error);
         this.stack = null;
     }
 }
