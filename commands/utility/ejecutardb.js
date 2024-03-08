8378cfac05e70406e438e503b64563d2354504c1
elmanueh <bernabe2003@gmail.com>
2024-03-08 21:04:41 +0100

    Refactor command to execute in database

--- commands/utility/ejecutardb.js
+++ commands/utility/ejecutardb.js
@@ -1,8 +1,9 @@
 import { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
 import { closeConnectionDatabase, openConnectionDatabase, runDatabase } from '../../src/services/database.js';
 import { writeConsoleANDLog } from '../../src/write.js';
-import { discordOwnerId_elmanueh } from '../../src/services/discord.js';
 import mensajes from '../../src/locale.json' assert { type: 'json' };
+
+const ELMANUEH_DISCORD_ID = '219739628196855808';
 
 export default {
 	category: 'utility',
@@ -16,26 +17,24 @@ export default {
 	async execute(interaction) {
         const db = await openConnectionDatabase();
         try {
-            const databaseRequest = interaction.options.getString('ejecutar');
-            if (interaction.user.id !== discordOwnerId_elmanueh) return interaction.reply({ content: mensajes.discord.permisos_insuficientes, ephemeral: true });
-
-            const rollbackButton = new ButtonBuilder()
-                .setCustomId('rollback')
-                .setLabel('Rollback')
-                .setStyle(ButtonStyle.Danger);
-
+            const queryDatabase = interaction.options.getString('ejecutar');
+            if (interaction.user.id !== ELMANUEH_DISCORD_ID) return interaction.reply({ content: mensajes.discord.permisos_insuficientes, ephemeral: true });
+            
             const commitButton = new ButtonBuilder()
                 .setCustomId('commit')
                 .setLabel('Commit')
                 .setStyle(ButtonStyle.Success);
-
-            const row = new ActionRowBuilder()
+            const rollbackButton = new ButtonBuilder()
+                .setCustomId('rollback')
+                .setLabel('Rollback')
+                .setStyle(ButtonStyle.Danger);
+            const rowButtons = new ActionRowBuilder()
                 .addComponents(commitButton, rollbackButton);
             
-            await runDatabase(db, 'BEGIN');
-            await runDatabase(db, databaseRequest);
+            await runDatabase(db, 'BEGIN EXCLUSIVE');
+            await runDatabase(db, queryDatabase);
 
-            const messageInteraction = await interaction.reply({ content: '¿Estás seguro? Comprueba que los cambios son los esperados. (30 seg)', components: [row], ephemeral: true });
+            const messageInteraction = await interaction.reply({ content: '¿Estás seguro? Comprueba que los cambios son los esperados. (30 seg)', components: [rowButtons], ephemeral: true });
             try {
                 const buttonPressed = await messageInteraction.awaitMessageComponent({ time: 30_000 });
                 if (buttonPressed.customId === 'commit') {
@@ -49,12 +48,12 @@ export default {
                 await runDatabase(db, 'ROLLBACK');
                 await interaction.editReply({ content: 'No se ha recibido ninguna respuesta, cancelando operación.', components: [] });
             }
-            await closeConnectionDatabase(db);
         } catch (error) {
             await runDatabase(db, 'ROLLBACK');
-            await closeConnectionDatabase(db);
             await interaction.reply({ content: mensajes.error.notificar, ephemeral: true });
             await writeConsoleANDLog(error);
+        } finally {
+            await closeConnectionDatabase(db);
         }
     }
 };
