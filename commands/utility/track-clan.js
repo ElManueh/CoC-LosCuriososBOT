7e1bd99989655cdeed589aa46824ca9793730109
elmanueh <bernabe2003@gmail.com>
2024-03-09 11:16:13 +0100

    Refactor command track a clan

--- commands/utility/track-clan.js
+++ commands/utility/track-clan.js
@@ -3,7 +3,7 @@ import { getClan } from '../../src/services/clashofclansAPI.js';
 import { closeConnectionDatabase, openConnectionDatabase, runDatabase } from '../../src/services/database.js';
 import mensajes from '../../src/locale.json' assert { type: 'json' };
 import { writeConsoleANDLog } from '../../src/write.js';
-import { ClashOfClansError, DatabaseError, SQLITE_CONSTRAINT_FOREIGNKEY, SQLITE_CONSTRAINT_UNIQUE } from '../../src/errorCreate.js';
+import { ClashOfClansError, DatabaseError, SQLITE_BUSY_TIMEOUT, SQLITE_CONSTRAINT_FOREIGNKEY, SQLITE_CONSTRAINT_UNIQUE } from '../../src/errorCreate.js';
 
 export default {
 	category: 'utility',
@@ -17,34 +17,33 @@ export default {
 	async execute(interaction) {
 		const db = await openConnectionDatabase();
 		try {
-			let optionClanTag = interaction.options.getString('clan-tag');
-			let clan = await getClan(optionClanTag);
+			const optionClanTag = interaction.options.getString('clan-tag');
+			const clan = await getClan(optionClanTag);
 
-			await runDatabase(db, 'BEGIN');
+			await runDatabase(db, 'BEGIN IMMEDIATE');
 			try {
 				await runDatabase(db, `INSERT INTO GuildConnections (guildId, clan) VALUES ('${interaction.guild.id}', '${clan.tag}')`);
 			} catch (error) {
-				
 				if (error instanceof DatabaseError) {
-					if (error.code === SQLITE_CONSTRAINT_UNIQUE) return await interaction.reply({ content: mensajes.clashofclans.tag_ya_vinculado, ephemeral: true });
-					if (error.code === SQLITE_CONSTRAINT_FOREIGNKEY) {	// clan no creado
+					if (error.code === SQLITE_CONSTRAINT_UNIQUE) return interaction.reply({ content: mensajes.clashofclans.tag_ya_vinculado, ephemeral: true });
+					if (error.code === SQLITE_CONSTRAINT_FOREIGNKEY) {
 						await runDatabase(db, `INSERT INTO ClanData (tag) VALUES ('${clan.tag}')`);
 						await runDatabase(db, `INSERT INTO GuildConnections (guildId, clan) VALUES ('${interaction.guild.id}', '${clan.tag}')`);
 					}
+					if (error.code === SQLITE_BUSY_TIMEOUT) throw error;
 				}
 			}
 			await runDatabase(db, 'COMMIT');
-
 			await interaction.reply({ content: 'Clan vinculado al servidor correctamente. Recuerda configurarlo para tener acceso a las distintas funcionalidades.', ephemeral: true });
-			await closeConnectionDatabase(db);
 		} catch (error) {
-			await closeConnectionDatabase(db);
-			if (error instanceof ClashOfClansError) {
-				if (error.errno === 404) return await interaction.reply({ content: mensajes.clashofclans.tag_incorrecto, ephemeral: true });
-			}
-
-			await interaction.reply({ content: mensajes.error.notificar, ephemeral: true });
+			await runDatabase(db, 'ROLLBACK');
 			await writeConsoleANDLog(error);
+			if (error instanceof ClashOfClansError) {
+				if (error.errno === 404) return interaction.reply({ content: mensajes.clashofclans.tag_incorrecto, ephemeral: true });
+			}
+			await interaction.reply({ content: mensajes.error.notificar, ephemeral: true });
+		} finally {
+			await closeConnectionDatabase(db);
 		}
 	},
 };
