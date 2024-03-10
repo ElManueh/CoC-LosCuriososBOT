447a8afb5422663bef4560ebc41888059b3ab3ba
elmanueh <bernabe2003@gmail.com>
2024-03-10 18:47:09 +0100

    New error handler

--- src/errorCreate.js
+++ src/errorCreate.js
@@ -1,53 +1,70 @@
-function getClashOfClansErrorMessage(error) {
-    let errno = error.response.status;
-    if (errno === 400) return 'Client provided incorrect parameters for the request.';
-    if (errno === 403) return 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.';
-    if (errno === 404) return 'Resource was not found.';
-    if (errno === 429) return 'Request was throttled, because amount of requests was above the threshold defined for the used API token.';
-    if (errno === 500) return 'Unknown error happened when handling the request.';
-    if (errno === 503) return 'Service is temprorarily unavailable because of maintenance.';
-    if (errno === 504) return 'The server did not get a response in time from the upstream server that it needed in order to complete the request.'
-}
+const CLASHOFCLANS_ERROR = 'CLASHOFCLANS_ERROR';
+export const HTTP_400_BAD_REQUEST = 400;
+export const HTTP_403_FORBIDDEN = 403;
+export const HTTP_404_NOT_FOUND = 404;
+export const HTTP_429_TOO_MANY_REQUESTS = 429;
+export const HTTP_500_INTERNAL_SERVER_ERROR = 500;
+export const HTTP_503_SERVICE_UNAVAILABLE = 503;
+export const HTTP_504_GATEWAY_TIMEOUT = 504;
 
 export class ClashOfClansError extends Error {
     constructor (error) {
-        super(getClashOfClansErrorMessage(error));
-        this.name = 'CLASHOFCLANS_ERROR';
-        this.errno = error.response.status;
-        this.stack = null;
+        super(ClashOfClansError.#getErrorMessage(error));
+        this.name = CLASHOFCLANS_ERROR;
+        this.errno = error.response ? error.response.status : -1;
+    }
+    
+    static #getErrorMessage(error) {
+        if (!error.response || !error.response.status) return 'Unknown error occurred.'
+        let errno = error.response.status;
+
+        switch (errno) {
+            case HTTP_400_BAD_REQUEST: return 'Client provided incorrect parameters for the request.';
+            case HTTP_403_FORBIDDEN: return 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.';
+            case HTTP_404_NOT_FOUND: return 'Resource was not found.';
+            case HTTP_429_TOO_MANY_REQUESTS: return 'Request was throttled, because amount of requests was above the threshold defined for the used API token.';
+            case HTTP_500_INTERNAL_SERVER_ERROR: return 'Unknown error happened when handling the request.';
+            case HTTP_503_SERVICE_UNAVAILABLE: return 'Service is temprorarily unavailable because of maintenance.';
+            case HTTP_504_GATEWAY_TIMEOUT: return 'The server did not get a response in time from the upstream server that it needed in order to complete the request.'
+            default: return 'Unknown error occurred.';
+        }
     }
 }
 
-const SQLITE_CONSTRAINT = 19;
+
+
+const DATABASE_ERROR = 'DATABASE_ERROR';
 const SQLITE_BUSY = 5;
+const SQLITE_CONSTRAINT = 19;
+export const SQLITE_BUSY_TIMEOUT = 'SQLITE_BUSY_TIMEOUT';
 export const SQLITE_CONSTRAINT_FOREIGNKEY = 'SQLITE_CONSTRAINT_FOREIGNKEY';
 export const SQLITE_CONSTRAINT_UNIQUE = 'SQLITE_CONSTRAINT_UNIQUE';
-export const SQLITE_BUSY_TIMEOUT = 'SQLITE_BUSY_TIMEOUT';
 export const SQLITE_ERROR = 'SQLITE_ERROR';
-
-function getDatabaseError(error) {
-    let errno = error.errno;
-    switch(errno) {  
-        case SQLITE_CONSTRAINT:
-            let message = error.message;
-            if (message.indexOf('FOREIGN KEY') !== -1) {
-                return SQLITE_CONSTRAINT_FOREIGNKEY;
-            } else if (message.indexOf('UNIQUE') !== -1) {
-                return SQLITE_CONSTRAINT_UNIQUE;
-            }
-        case SQLITE_BUSY:
-            return SQLITE_BUSY_TIMEOUT;
-        default:
-            return SQLITE_ERROR;
-    }
-}
 
 export class DatabaseError extends Error {
     constructor (error) {
-        super(error.message);
-        this.name = 'DATABASE_ERROR';
-        this.errno = error.errno;
-        this.code = getDatabaseError(error);
-        this.stack = null;
+        super(error && error.message ? error.message : 'Unknown database error');
+        this.name = DATABASE_ERROR;
+        this.errno = error && error.errno ? error.errno : -1;
+        this.code = DatabaseError.#getExtendedError(error);
+    }
+
+    static #getExtendedError(error) {
+        if (!error || !error.errno) return SQLITE_ERROR;
+        let errno = error.errno;
+        
+        switch(errno) {  
+            case SQLITE_CONSTRAINT:
+                if (error.message && /FOREIGN KEY/.test(error.message)) {
+                    return SQLITE_CONSTRAINT_FOREIGNKEY;
+                } else if (error.message && /UNIQUE/.test(error.message)) {
+                    return SQLITE_CONSTRAINT_UNIQUE;
+                }
+                break;
+            case SQLITE_BUSY:
+                return SQLITE_BUSY_TIMEOUT;
+            default:
+                return SQLITE_ERROR;
+        }
     }
 }
