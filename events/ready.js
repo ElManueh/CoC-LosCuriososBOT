7229560d7cb1ae408d32b50f52657835a2c51b3d
elmanueh <bernabe2003@gmail.com>
2024-03-13 20:16:11 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,23 @@
 import { Events } from 'discord.js';
+import * as Database from '../src/services/database.js';
 import { currentWar } from '../src/events/currentWar.js';
 import { databaseUpdate } from '../src/events/databaseUpdate.js';
-import { discordGuildId } from '../src/services/discord.js';
-import { RequestCountClashOfClansCreate } from '../src/services/clashofclansAPI.js';
 import { writeConsoleANDLog } from '../src/write.js';
-import { createDatabase } from '../src/services/database.js';
 
 export default {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		await writeConsoleANDLog('-------------------------------------------------');
-		await writeConsoleANDLog(`Conectado correctamente como '${client.user.tag}'`);
+		await writeConsoleANDLog(`Connected correctly as '${client.user.tag}'`);
 
 		try {
-			await createDatabase();
-		} catch (error) { return await writeConsoleANDLog('PLEASE, RESTART THE BOT'); }
+			await Database.initialize();
+		} catch (error) { return writeConsoleANDLog('PLEASE, RESTART THE BOT'); }
 
 		// This execute the code to manage the database update with information about wars.
 		currentWar();
 		// This execute the code to manage the update of users clan.
 		databaseUpdate();
-		// Initialize limit rate of api coc.
-		RequestCountClashOfClansCreate();
 	},
 };
