397090f9f6b8afec9b81c1ff16fb80a1532b3207
elmanueh <bernabe2003@gmail.com>
2024-03-13 17:42:18 +0100

    Module for API clashofclans

--- src/services/clashofclansAPI.js
+++ src/services/clashofclansAPI.js
@@ -1,86 +1,65 @@
-import axios from 'axios';
+import * as AxiosAdapter from '../services/axios-adapter.js';
 import { ClashOfClansError } from '../errorCreate.js';
-import { config } from 'dotenv';
-config();
 
+// Starting module
+const LINK_API = 'https://api.clashofclans.com/v1';
+const MAX_LIMIT_REQUEST = 30;
 let requestCount = 0;
-export async function RequestCountClashOfClansCreate() {
-  setInterval(() => { requestCount = 0 }, 1000);
-}
+setInterval(() => { requestCount = 0; }, 1000);
 
-async function RequestCount() {
-  while (requestCount >= 30) await new Promise(resolve => setTimeout(resolve, 1000));
+// Add a new request to the API
+async function requestCountAPI() {
+  while (requestCount >= MAX_LIMIT_REQUEST) await new Promise(resolve => setTimeout(resolve, 1000));
   requestCount++;
-}
-
-// Configuration API request
-const axiosConfig = {
-  headers: {
-    'Authorization': `Bearer ${process.env.API_KEY}`,
-    'Accept': 'application/json',
-  },
-};
-
-// Get request
-async function requestApiGet(uri) {
-  try {
-    await RequestCount();
-    const responseApi = await axios.get(uri, axiosConfig);
-    return responseApi.data;
-  } catch (error) { throw error; }
-}
-
-// Post request
-async function requestApiPost(uri, data) {
-  try {
-    await RequestCount();
-    const responseApi = await axios.post(uri, data, axiosConfig);
-    return responseApi.data;
-  } catch (error) { throw error; }
 }
 
 // Get player info
 export async function getPlayer(playerTag) {
-  const uri = `${process.env.LINK_API}/players/${encodeURIComponent(playerTag)}`;
+  const uri = `${LINK_API}/players/${encodeURIComponent(playerTag)}`;
   try {
-    const player = await requestApiGet(uri);
+    await requestCountAPI();
+    const player = await AxiosAdapter.requestApiGet(uri);
     return player;
   } catch (error) { throw new ClashOfClansError(error); }
 }
 
 // Get clan info
 export async function getClan(clanTag) {
-  const uri = `${process.env.LINK_API}/clans/${encodeURIComponent(clanTag)}`;
+  const uri = `${LINK_API}/clans/${encodeURIComponent(clanTag)}`;
   try {
-    const clan = await requestApiGet(uri);
+    await requestCountAPI();
+    const clan = await AxiosAdapter.requestApiGet(uri);
     return clan;
   } catch (error) { throw new ClashOfClansError(error); }
 }
 
 // Verify player account with his token
 export async function verifyPlayerToken(playerTag, playerToken) {
-  const uri = `${process.env.LINK_API}/players/${encodeURIComponent(playerTag)}/verifytoken`;
+  const uri = `${LINK_API}/players/${encodeURIComponent(playerTag)}/verifytoken`;
   const data = { "token": `${playerToken}` };
   try {
-    const tokenInfo = await requestApiPost(uri, data);
+    await requestCountAPI();
+    const tokenInfo = await AxiosAdapter.requestApiPost(uri, data);
     return tokenInfo.status === 'ok' ? true : false;
   } catch (error) { throw new ClashOfClansError(error); }
 }
 
 // Get clan players
 export async function getClanPlayers(clan) {
-  const uri = `${process.env.LINK_API}/clans/${encodeURIComponent(clan)}/members`;
+  const uri = `${LINK_API}/clans/${encodeURIComponent(clan)}/members`;
   try {
-    const players = await requestApiGet(uri);
+    await requestCountAPI();
+    const players = await AxiosAdapter.requestApiGet(uri);
     return players.items;
   } catch (error) { throw new ClashOfClansError(error); }
 }
 
 // Get currentWar for clan
 export async function getClanCurrentWar(clan) {
-  const uri = `${process.env.LINK_API}/clans/${encodeURIComponent(clan)}/currentwar`;
+  const uri = `${LINK_API}/clans/${encodeURIComponent(clan)}/currentwar`;
   try {
-    const currentWar = await requestApiGet(uri);
+    await requestCountAPI();
+    const currentWar = await AxiosAdapter.requestApiGet(uri);
     return currentWar;
   } catch (error) { throw new ClashOfClansError(error); }
 }
